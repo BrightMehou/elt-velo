b42d95a1e64c1051b54270600bd852af977c3f78
BrightMehou <joanemehou@gmail.com>
2025-09-03 12:34:10 +0200

    refactor of consolidate_station and stg_station models

--- src/elt/models/consolidate/consolidate_station.sql
+++ src/elt/models/consolidate/consolidate_station.sql
@@ -3,23 +3,41 @@
     unique_key = ['id','created_date'],
   )
 }}
+with city_codes_cte as (
+        select
+            id as insee_code,
+            name as city_name,
+            created_date
+        from
+            {{ ref('consolidate_city') }}
+        where
+            created_date = (
+                select
+                    max(created_date)
+                from
+                    {{ ref('consolidate_city') }}
+            )
+            and
+            name in ('nantes', 'strasbourg', 'toulouse')
+    )
 select
-   id,
-   code,
-   name,
-   city_name,
-   city_code,
-   address,
-   longitude,
-   latitude,
-   status,
-   created_date,
-   capacity,
+   source.id,
+   source.code,
+   source.name,
+   source.city_name,
+   coalesce(source.city_code, c.insee_code) as city_code,
+   source.address,
+   source.longitude,
+   source.latitude,
+   source.status,
+   source.created_date,
+   source.capacity,
 from
-     {{ ref('stg_station') }}
+     {{ ref('stg_station') }} as source
+left join city_codes_cte c on source.city_name = c.city_name
 
 {% if is_incremental() %}
 
-where created_date >= (select coalesce(max(created_date),'1900-01-01') from {{ this }} )
+where source.created_date >= (select coalesce(max(created_date),'1900-01-01') from {{ this }} )
 
 {% endif %}
--- src/elt/models/staging/stg_station.sql
+++ src/elt/models/staging/stg_station.sql
@@ -1,18 +1,3 @@
-with city_codes_cte as (
-        select
-            id as insee_code,
-            name as city_name,
-            created_date
-        from
-            {{ ref('consolidate_city') }}
-        where
-            created_date = (
-                select
-                    max(created_date)
-                from
-                    {{ ref('consolidate_city') }}
-            )
-    ) -- Paris sans jointure car déjà code_insee_commune dans JSON
 select
     '{{ var("PARIS_CITY_CODE", "1") }}' || '-' || stationcode as id,
     stationcode as code,
@@ -30,57 +15,54 @@ from
         's3://bicycle-data/{{ run_started_at.strftime("%Y-%m-%d") }}/paris_realtime_bicycle_data.json'
     )
 union
-all -- Nantes avec jointure city_code
+all 
 select
-    '{{ var("NANTES_CITY_CODE", "2") }}' || '-'  || source.number as id,
-    source.number as code,
-    source.name,
-    source.contract_name as city_name,
-    c.insee_code as city_code,
-    source.address,
-    source.position.lon as longitude,
-    source.position.lat as latitude,
-    source.status,
+    '{{ var("NANTES_CITY_CODE", "2") }}' || '-'  || number as id,
+    number as code,
+    name,
+    contract_name as city_name,
+    null as city_code,
+    address,
+    position.lon as longitude,
+    position.lat as latitude,
+    status,
     current_date() as created_date,
-    source.bike_stands as capacity
+    bike_stands as capacity
 from
     read_json(
         's3://bicycle-data/{{ run_started_at.strftime("%Y-%m-%d") }}/nantes_realtime_bicycle_data.json'
-    ) as source
-    left join city_codes_cte c on source.contract_name = c.city_name
+    ) 
 union
-all -- Toulouse avec jointure city_code
+all 
 select
-    '{{ var("TOULOUSE_CITY_CODE", "3") }}' || '-'  || source.number as id,
-    source.number as code,
-    source.name,
-    source.contract_name as city_name,
-    c.insee_code as city_code,
-    source.address,
-    source.position.lon as longitude,
-    source.position.lat as latitude,
-    source.status,
+    '{{ var("TOULOUSE_CITY_CODE", "3") }}' || '-'  || number as id,
+    number as code,
+    name,
+    contract_name as city_name,
+    null as city_code,
+    address,
+    position.lon as longitude,
+    position.lat as latitude,
+    status,
     current_date() as created_date,
-    source.bike_stands as capacity
+    bike_stands as capacity
 from
     read_json(
         's3://bicycle-data/{{ run_started_at.strftime("%Y-%m-%d") }}/toulouse_realtime_bicycle_data.json'
-    ) as source
-    left join city_codes_cte c on source.contract_name = c.city_name
+    ) 
 union
-all -- Strasbourg avec jointure city_code
+all 
 select
-    '{{ var("STRASBOURG_CITY_CODE", "4") }}' || '-'  || source.id as id,
-    source.id as code,
-    source.na as name,
+    '{{ var("STRASBOURG_CITY_CODE", "4") }}' || '-'  || id as id,
+    id as code,
+    na as name,
     'strasbourg' as city_name,
-    c.insee_code as city_code,
+    null as city_code,
     null as address,
-    source.lon as longitude,
-    source.lat as latitude,
-    source.is_installed as status,
+    lon as longitude,
+    lat as latitude,
+    is_installed as status,
     current_date() as created_date,
-    source."to" as capacity
+    "to" as capacity
 from
-    read_json('s3://bicycle-data/{{ run_started_at.strftime("%Y-%m-%d") }}/strasbourg_realtime_bicycle_data.json') as source
-left join city_codes_cte c on c.city_name = 'strasbourg'
+    read_json('s3://bicycle-data/{{ run_started_at.strftime("%Y-%m-%d") }}/strasbourg_realtime_bicycle_data.json') 
